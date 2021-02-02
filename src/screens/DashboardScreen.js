af2dc45f9e41a8f9575bff54731f14a531de3491
ericksonder <ericksonder017@gmail.com>
2021-02-02 18:33:31 -0500

    6:33pm 02-02-2021

--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import BarraDeEstadisticas  from "./../components/barraDeEstadisticas/BarraDeEstadisticas" 
 import HistoricoMensual from "./../components/historicoMensual/HistoricoMensual";
 import TablaCitas from "./../components/tablaCitas/TablaCitas";
 import "./../styles.css";
+import { getCitas } from "./../servicios/servicios"
 
 const DashboardScreen = () => {
+  
+  const [citas, setCitas] = useState([]);
+
+  useEffect(() => {
+    getCitas().then(data => {
+      console.log(data);
+      setCitas(data);
+    });
+  }, []);
+  
   return (
     <div className="content__wrapper">
       <div className="row">
@@ -16,7 +27,7 @@ const DashboardScreen = () => {
       <div className="row bg-gray p-5">
 
         <div className="col-xl-8 col-md-12 col-sm-12">
-          <TablaCitas />
+          <TablaCitas citas={citas}/>
         </div>
 
         <div className="col-xl-4 col-md-12 col-sm-12">
---
+++ src/servicios/servicios.js
@@ -0,0 +1,70 @@
+import { URL_BACKEND1, URL_BACKEND2} from "./../environments/environments";
+
+
+export const getUsuarios = async() => {
+    const peticion = await fetch(`${URL_BACKEND1}/users`);
+    const data = await peticion.json();
+    return data;
+}
+
+export const getCitas = async() => {
+    const peticion = await fetch(`${URL_BACKEND1}/citas`);
+    const data = await peticion.json();
+    return data;
+}
+
+export const getComentarios = async() => {
+    const peticion = await fetch(`${URL_BACKEND1}/comentarios`);
+    const data = await peticion.json();
+    return data;
+}
+
+export const postComentario = async(objComentario) => {
+    const peticion = await fetch(`${URL_BACKEND1}/comentarios`, {
+        method: "POST",
+        body: JSON.stringify(objComentario), 
+        headers: {
+            "Content-type": "application/json"
+        }
+    })
+    const data = await peticion.json();
+    return data
+}
+
+
+
+export const getVeterinarios = async () => {
+    const peticion = await fetch(`${URL_BACKEND1}/veterinarios`);
+    const data = await peticion.json();
+    return data;
+}
+
+export const postVeterinario = async (objVeterinario) => {
+    const peticion = await fetch(`${URL_BACKEND1}/veterinariso`, {
+        method: "POST",
+        body: JSON.stringify(objVeterinario),
+        headers: {
+            "Content-type": "application/json"
+        }
+    })
+    const data = await peticion.json();
+    return data
+}
+
+export const getDatosConsultorio = async () => {
+    const peticion = await fetch(`${URL_BACKEND2}/Consultorio`);
+    const data = await peticion.json();
+    return data; 
+}
+
+export const putDatosConsultorio = async (objDatosConsultorio)  => {
+    const peticion = await fetch(`${URL_BACKEND2}/Consultorio/${objDatosConsultorio.prop_id}`, {
+        method: "PUT", 
+        body: JSON.stringify(objDatosConsultorio),
+        headers: {
+            "Content-type": "application/json"
+        }
+    })
+    const data = await peticion.json();
+    return data
+}
