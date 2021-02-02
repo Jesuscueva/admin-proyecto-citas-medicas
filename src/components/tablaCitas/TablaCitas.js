5e439f0f32f89747a60f6b9021d5bdd44027fd9d
ericksonder <ericksonder017@gmail.com>
2021-02-02 18:24:16 -0500

    Servicios creados y la tabla ya recibe y muestra los datos

--- src/components/tablaCitas/TablaCitas.js
+++ src/components/tablaCitas/TablaCitas.js
@@ -1,10 +1,35 @@
 import React from 'react'
 import './tablaCitas.css'
 
-const TablaCitas = () => {
+const TablaCitas = ({ citas }) => {
     return (
         <div className="tablaCitas__wrapper shadow">
-            <h1>Tabla Citas</h1>
+            <h1>Citas</h1>
+            <table className="table table-striped table-hover table-bordered">
+                <thead className="table-light" >
+                    <tr>
+                        <th>Id</th>
+                        <th>Id de Usuario</th>
+                        <th>Fecha</th>
+                        <th>Especialidad</th>
+                        <th>Estado</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {
+                    citas.map(cita => {
+                        return (<tr>
+                            <th>{cita.id}</th>
+                            <td>{cita.userId}</td>
+                            <td>{cita.fecha}</td>
+                            <td>{cita.especialidad}</td>
+                            <td>{cita.estado}</td>
+                        </tr>)
+                    })
+                }    
+                </tbody>
+            </table>    
+
         </div>
     )
 }
